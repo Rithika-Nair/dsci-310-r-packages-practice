7fd8f42b0b702946ede700e695306f9f5f97c839
Rithika-Nair <rrithikanair@gmail.com>
2023-04-02 04:05:51 -0700

    edited DESCRIPTION, added license and documentation

--- R/str_split_one.R
+++ R/str_split_one.R
@@ -1,3 +1,14 @@
+#' Split a string
+#'
+#' @param x A character vector with one element.
+#' @param split What to split on.
+#'
+#' @return A character vector.
+#' @export
+#'
+#' @examples
+#' x <- "alfa,bravo,charlie,delta"
+#' str_split_one(x, split = ",")
 str_split_one <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
